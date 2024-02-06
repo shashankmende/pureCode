e2a82423d8327813a14607f1686537fe1282fe32
Shashank <shashankmende88@gmail.com>
2024-02-06 18:11:09 +0530

    purecode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,327 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-container">
+
+      <div className='extra-large-devices'>
+          <div className='extra-large-section-container'>
+              <h1 className='about-heading'>About Us</h1>
+              <p className='about-para'>It includes rich features & contents.It's designed & developed based on One Page/Mulit-page Layout blog themes. World Press
+                themes and blogspot . You can use any layout from any demo anywhere.
+
+              </p>
+              <p className='about-para'>webblogoverflow is completely creative. clean & 100% responsive website.  Put your business into next level with webublogoverflow.</p>
+
+              <h1 className='about-heading'>Social Links</h1>
+              <div className='social-buttons-container'>
+                  <button type='button' className='facebook-button'>Facebook</button>
+                  <button type='button' className='facebook-button'>Twitter</button>
+                  <button type='button' className='facebook-button'>Pinterest</button>
+                  <button type='button' className='facebook-button'>YouTube</button>
+                  <button type='button' className='facebook-button'>Linkedin</button>
+                
+              
+              </div>
+          </div>
+
+
+<div>
+
+      <div>
+          <div className='widgets-container'>
+              <h1 className='about-heading'>Tags Widget</h1>
+              <div>
+              <button type='button' className='facebook-button'>Classy</button>
+                  <button type='button' className='facebook-button'>Blog</button>
+                  <button type='button' className='facebook-button'>Creative</button>
+                  <button type='button' className='facebook-button'>One Page</button>
+                  <button type='button' className='facebook-button'>Multipurpose</button>
+                  <button type='button' className='facebook-button'>Minimal</button>
+                  <button type='button' className='facebook-button'>Classic</button>
+                  <button type='button' className='facebook-button'>Medical</button>
+              </div>
+          </div>
+        
+        </div>
+
+
+
+
+            <div className='important-container'>
+                <h1 className='about-heading'>Important Links</h1>
+                <div className='important-links-containe'>
+                  <div>
+                        <p className='about-para'>Terms & Conditions</p>
+                        <hr className='horizontal-line'/>
+                  </div>
+                  <div>
+                        <p className='about-para'>About Lincences</p>
+                        <hr className='horizontal-line'/>
+                  </div>
+                  <div>
+                        <p className='about-para'>Help & Support</p>
+                        <hr className='horizontal-line'/>
+                  </div>
+                  <div>
+                        <p className='about-para'>Careers</p>
+                        <hr className='horizontal-line'/>
+                  </div>
+                  <div>
+                        <p className='about-para'>Privacy Policy</p>
+                        <hr className='horizontal-line'/>
+                  </div>
+                  <div>
+                        <p className='about-para'>Community & Forum</p>
+                        <hr className='horizontal-line'/>
+                  </div>
+                  
+                </div>
+          </div>
+          </div>
+          <div>
+
+          <h1 className='about-heading'>Latest News</h1>
+        <div className='latest-news-container large-latest-container'>
+            <div className='each-card-container'>
+                <img src='https://res.cloudinary.com/dvvhafkyv/image/upload/v1706263202/Group_7732book_hub_logo_ogos6t.png' 
+                className='blog-image'
+                alt='blog-item'/>
+                <div className='new-container'>
+                <p className='each-card-heading'>Your Blog Title Goes Here</p>
+                <p className='data-para'>21 Jan 2018</p>
+                <p className='each-card-last-para'>Lorem ipsum dolor it amet, consectetur</p>
+                </div>
+            </div>
+            <div className='each-card-container'>
+                <img src='https://res.cloudinary.com/dvvhafkyv/image/upload/v1706263202/Group_7732book_hub_logo_ogos6t.png' 
+                className='blog-image'
+                alt='blog-item'/>
+                <div className='new-container'>
+                  <p className='each-card-heading'>Your Blog Title Goes Here</p>
+                  <p className='data-para'>21 Jan 2018</p>
+                  <p className='each-card-last-para'>Lorem ipsum dolor it amet, consectetur</p>
+                </div>
+            </div>
+            <div className='each-card-container'>
+                <img src='https://res.cloudinary.com/dvvhafkyv/image/upload/v1706263202/Group_7732book_hub_logo_ogos6t.png' 
+                className='blog-image'
+                alt='blog-item'/>
+                <div className='new-container'>
+                  <p className='each-card-heading'>Your Blog Title Goes Here</p>
+                  <p className='data-para'>21 Jan 2018</p>
+                  <p className='each-card-last-para'>Lorem ipsum dolor it amet, consectetur</p>
+                </div>
+            </div>
+        </div>
+
+
+
+          </div>
+ 
+
+          <div className='phone-and-email-container'>
+              <h1 className='about-heading'>Quick Contact</h1>
+              <div className='phone-container'>
+                  <p className='phone-para'>Phone:</p>
+              <div>
+                <p className='phone-numbers'>+255 789 54 50 40</p>
+                <p  className='phone-numbers'>+2255 788 90 94 00</p>
+              </div>
+              </div>
+              
+              <div className='phone-container'>
+              <p className='phone-para'>Email:</p>
+              <div>
+
+                <p  className='phone-numbers'>support@webblogoverflow.com</p>
+                <p   className='phone-numbers'>luckymashy@gmail.com</p>
+              </div>
+            </div>
+      
+      
+ 
+      </div>
+
+
+      
+
+      </div>
+
+
+
+      <div className='main-container'>
+      <div className='section-container'>
+          <h1 className='about-heading'>About Us</h1>
+          <p className='about-para'>It includes rich features & contents.It's designed & developed based on One Page/Mulit-page Layout blog themes. World Press
+            themes and blogspot . You can use any layout from any demo anywhere.
+
+          </p>
+          <p className='about-para'>webblogoverflow is completely creative. clean & 100% responsive website.  Put your business into next level with webublogoverflow.</p>
+
+          <h1 className='about-heading'>Social Links</h1>
+          <div className='social-buttons-container'>
+              <button type='button' className='facebook-button'>Facebook</button>
+              <button type='button' className='facebook-button'>Twitter</button>
+              <button type='button' className='facebook-button'>Pinterest</button>
+              <button type='button' className='facebook-button'>YouTube</button>
+              <button type='button' className='facebook-button'>Linkedin</button>
+            
+          
+          </div>
+
+
+          <div className=' large-news-container'>
+          <h1 className='about-heading'>Latest News</h1>
+        <div className='latest-news-container large-latest-container'>
+            <div className='each-card-container'>
+                <img src='https://res.cloudinary.com/dvvhafkyv/image/upload/v1706263202/Group_7732book_hub_logo_ogos6t.png' 
+                className='blog-image'
+                alt='blog-item'/>
+                <div className='new-container'>
+                <p className='each-card-heading'>Your Blog Title Goes Here</p>
+                <p className='data-para'>21 Jan 2018</p>
+                <p className='each-card-last-para'>Lorem ipsum dolor it amet, consectetur</p>
+                </div>
+            </div>
+            <div className='each-card-container'>
+                <img src='https://res.cloudinary.com/dvvhafkyv/image/upload/v1706263202/Group_7732book_hub_logo_ogos6t.png' 
+                className='blog-image'
+                alt='blog-item'/>
+                <div className='new-container'>
+                  <p className='each-card-heading'>Your Blog Title Goes Here</p>
+                  <p className='data-para'>21 Jan 2018</p>
+                  <p className='each-card-last-para'>Lorem ipsum dolor it amet, consectetur</p>
+                </div>
+            </div>
+            <div className='each-card-container'>
+                <img src='https://res.cloudinary.com/dvvhafkyv/image/upload/v1706263202/Group_7732book_hub_logo_ogos6t.png' 
+                className='blog-image'
+                alt='blog-item'/>
+                <div className='new-container'>
+                  <p className='each-card-heading'>Your Blog Title Goes Here</p>
+                  <p className='data-para'>21 Jan 2018</p>
+                  <p className='each-card-last-para'>Lorem ipsum dolor it amet, consectetur</p>
+                </div>
+            </div>
+        </div>
+        </div>
+
+
+
+
+          </div>
+
+<div className='large-widgets-importaint-latest-news-container'>
+
+
+  <div className='widgets-container'>
+          <h1 className='about-heading'>Tags Widget</h1>
+          <div>
+          <button type='button' className='facebook-button'>Classy</button>
+              <button type='button' className='facebook-button'>Blog</button>
+              <button type='button' className='facebook-button'>Creative</button>
+              <button type='button' className='facebook-button'>One Page</button>
+              <button type='button' className='facebook-button'>Multipurpose</button>
+              <button type='button' className='facebook-button'>Minimal</button>
+              <button type='button' className='facebook-button'>Classic</button>
+              <button type='button' className='facebook-button'>Medical</button>
+          </div>
+          </div>
+
+
+
+          <div className='important-container'>
+          <h1 className='about-heading'>Important Links</h1>
+          <div className='important-links-containe'>
+            <div>
+                  <p className='about-para'>Terms & Conditions</p>
+                  <hr className='horizontal-line'/>
+            </div>
+            <div>
+                  <p className='about-para'>About Lincences</p>
+                  <hr className='horizontal-line'/>
+            </div>
+            <div>
+                  <p className='about-para'>Help & Support</p>
+                  <hr className='horizontal-line'/>
+            </div>
+            <div>
+                  <p className='about-para'>Careers</p>
+                  <hr className='horizontal-line'/>
+            </div>
+            <div>
+                  <p className='about-para'>Privacy Policy</p>
+                  <hr className='horizontal-line'/>
+            </div>
+            <div>
+                  <p className='about-para'>Community & Forum</p>
+                  <hr className='horizontal-line'/>
+            </div>
+            
+          </div>
+          </div>
+
+          <div className='latest-news-bg-container'>
+          <h1 className='about-heading  latest-heading'>Latest News</h1>
+        <div className='latest-news-container'>
+            <div className='each-card-container'>
+                <img src='https://res.cloudinary.com/dvvhafkyv/image/upload/v1706263202/Group_7732book_hub_logo_ogos6t.png' 
+                className='blog-image'
+                alt='blog-item'/>
+                <div className='new-container'>
+                <p className='each-card-heading'>Your Blog Title Goes Here</p>
+                <p className='data-para'>21 Jan 2018</p>
+                <p className='each-card-last-para'>Lorem ipsum dolor it amet, consectetur</p>
+                </div>
+            </div>
+            <div className='each-card-container'>
+                <img src='https://res.cloudinary.com/dvvhafkyv/image/upload/v1706263202/Group_7732book_hub_logo_ogos6t.png' 
+                className='blog-image'
+                alt='blog-item'/>
+                <div className='new-container'>
+                  <p className='each-card-heading'>Your Blog Title Goes Here</p>
+                  <p className='data-para'>21 Jan 2018</p>
+                  <p className='each-card-last-para'>Lorem ipsum dolor it amet, consectetur</p>
+                </div>
+            </div>
+            <div className='each-card-container'>
+                <img src='https://res.cloudinary.com/dvvhafkyv/image/upload/v1706263202/Group_7732book_hub_logo_ogos6t.png' 
+                className='blog-image'
+                alt='blog-item'/>
+                <div className='new-container'>
+                  <p className='each-card-heading'>Your Blog Title Goes Here</p>
+                  <p className='data-para'>21 Jan 2018</p>
+                  <p className='each-card-last-para'>Lorem ipsum dolor it amet, consectetur</p>
+                </div>
+            </div>
+        </div>
+        </div>
+        <div className='phone-and-email-container'>
+              <h1 className='about-heading'>Quick Contact</h1>
+              <div className='phone-container'>
+                  <p className='phone-para'>Phone:</p>
+              <div>
+                <p className='phone-numbers'>+255 789 54 50 40</p>
+                <p  className='phone-numbers'>+2255 788 90 94 00</p>
+              </div>
+              </div>
+              
+              <div className='phone-container'>
+              <p className='phone-para'>Email:</p>
+              <div>
+
+                <p  className='phone-numbers'>support@webblogoverflow.com</p>
+                <p   className='phone-numbers'>luckymashy@gmail.com</p>
+              </div>
+            </div>
+      
+      
+ 
+      </div>
+      </div>
+      </div>
     </div>
   );
 }
